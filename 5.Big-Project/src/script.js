3b9237cba61291f438f9d94d4ffa19e8f61642a2
Farhana Bente Islam <21303195@iubat.edu>
2024-06-17 23:59:01 +0600

    First Commit

---
+++ 5.Big-Project/src/script.js
@@ -0,0 +1,25 @@
+/*===========================================
+   Nav Login Button Toggle for mobile version
+============================================*/ 
+
+const login_btn = document.querySelector(".btn-log1");
+const navbar = document.querySelector(".navbar");
+const toggleLogin = ()=>{
+ navbar.classList.toggle("active");
+};
+
+login_btn.addEventListener('click', () => toggleLogin());
+
+
+
+/*===================================
+   Nav Toggle icon for mobile version
+=====================================*/ 
+const menu_btn = document.querySelector(".nav-btn");
+const nav_bar = document.querySelector(".body");
+const toggleMenuBtn = ()=>{
+   nav_bar.classList.toggle("active1");
+};
+
+menu_btn.addEventListener('click', () => toggleMenuBtn());
+
