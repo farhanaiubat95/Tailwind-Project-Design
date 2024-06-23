b2244ddd3fa58ac658e1872d9c0bbf557d383507
Farhana Bente Islam <21303195@iubat.edu>
2024-06-23 18:58:50 +0600

    first commit

--- 5.Big-Project/tailwind.config.js
+++ 5.Big-Project/tailwind.config.js
@@ -4,12 +4,13 @@ module.exports = {
   theme: {
     extend: {
     colors: {
-      "primary":"#0E606E",
+      "primary":"#003C43",
       "Secondary":"#9AB8BD",
       "3rd":"#6F9196",
       "4th":"#A3BEC1",
       "5th":"#e7f0ef",
-      "6th":"#DCF2F1"
+      "6th":"#DCF2F1",
+      "7th":"#0E606E"
     },
     fontFamily:{
       roboto: ['Roboto', 'sans-serif'],     
